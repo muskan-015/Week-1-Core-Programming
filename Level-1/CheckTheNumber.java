4b631b4634075065eb7e6780a9ee363406423e6c
muskan-015 <muskanvds2004@gmail.com>
2025-01-10 11:26:06 +0530

    Adding files

---
+++ Level-1/CheckTheNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CheckTheNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // To Check if the number is positive, negative, or zero
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        sc.close();
+    }
+}
